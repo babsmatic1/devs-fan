f3f3911427e64adb9375914b8b231f7241a58f04
Babatunde Abdullahi <ABDULLAHIBABATUNDE644@GMAIL.COM>
2024-02-23 04:11:53 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let a=document.getElementById("fan");
+
+function myfanon()
+{a.style.animationDuration= 3+"s";}
+
+function myfanoff()
+{a.style.animationDuration=0+"s";}
+
+function myfan1()
+{a.style.animationDuration=1+"s";}
+
+function myfan2()
+{a.style.animationDuration=0.5+"s";}
+
+function myfan3()
+{a.style.animationDuration= 0.1+"s";}
+
